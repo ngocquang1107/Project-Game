afb3f2649004409886ed8e67e29bfcd223b70278
Lê Ngọc Quang <ngocquangg1234@gmail.com>
2025-04-26 22:32:48 +0700

    Update character.h

--- character.h
+++ character.h
@@ -11,15 +11,18 @@ public:
     Character(SDL_Texture* charTex, SDL_Texture* arrTex, int x, int y);
     SDL_Texture* texture;
     SDL_Rect srcRect, destRect;
+    SDL_Rect collisionRect; // Vùng va chạm
     int frameWidth, frameHeight;
     int currentFrame, totalFrames;
     int frameTime, lastFrameTime;
+    int health; // Máu của nhân vật
+    bool isAlive; // Trạng thái sống
     std::vector<Arrow> arrows;
     SDL_Texture* arrowTexture;
     float x, y;
     float speed;
     double angle;
-    bool moveLeft, moveRight; // Chỉ giữ moveLeft, moveRight
+    bool moveLeft, moveRight;
     Uint32 lastShootTime; // Theo dõi thời gian bắn cuối cùng
     void handleInput(SDL_Event& event);
     void updateAnimation(int currentTime);
